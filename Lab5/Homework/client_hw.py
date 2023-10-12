2c7d370c345bbfa68dac6b7cdd9daff7d1b2c5ab
Dragonfly929 <zaica.maia3@gmail.com>
2023-10-12 11:24:04 +0300

    commit

---
+++ Lab5/Homework/client_hw.py
@@ -0,0 +1,111 @@
+import socket
+import threading
+import json
+import os
+import base64
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+if not os.path.exists("CLIENT_MEDIA"):
+    os.makedirs("CLIENT_MEDIA")
+
+
+def handle_downloaded_image(received_message):
+    image_name = received_message["payload"]["image_name"]
+
+    if image_name.startswith("The "):
+        print(image_name)
+    else:
+        image_data = received_message["payload"]["image_data"]
+        with open(f"CLIENT_MEDIA/{image_name}", "wb") as image_file:
+            image_file.write(base64.b64decode(image_data))
+        print(f"File {image_name} downloaded successfully.")
+
+
+def receive_messages():
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        received_message = json.loads(data.decode('utf-8'))
+        message_type = received_message["type"]
+        if message_type == "connect_ack":
+            print(received_message["payload"]["message"])
+        elif message_type == "message":
+            sender = received_message["payload"]["sender"]
+            room = received_message["payload"]["room"]
+            message_text = received_message["payload"]["text"]
+            print(f"[{room}] {sender}: {message_text}")
+        elif message_type == "notification":
+            notification_message = received_message["payload"]["message"]
+            print(f"Notification: {notification_message}")
+        elif message_type == "downloaded_image":
+            handle_downloaded_image(received_message)
+
+
+def send_message():
+    while True:
+        message = input(
+            "Enter a message (or 'upload: <path to image file>', 'download: <image name>', or 'exit' to quit): ")
+        if message.lower() == 'exit':
+            break
+        if message.startswith("upload: "):
+            image_path = message[8:]
+            if os.path.exists(image_path):
+                with open(image_path, "rb") as image_file:
+                    image_data = base64.b64encode(image_file.read()).decode('utf-8')
+                upload_data = {
+                    "type": "upload",
+                    "payload": {
+                        "image_name": os.path.basename(image_path),
+                        "image_data": image_data,
+                    }
+                }
+                client_socket.send(json.dumps(upload_data).encode('utf-8'))
+            else:
+                print(f"File {image_path} doesn't exist.")
+        elif message.startswith("download: "):
+            download_command = {
+                "type": "download",
+                "payload": {
+                    "image_name": message[10:],
+                }
+            }
+            client_socket.send(json.dumps(download_command).encode('utf-8'))
+        else:
+            message_data = {
+                "type": "message",
+                "payload": {
+                    "sender": name,
+                    "room": room,
+                    "text": message,
+                },
+            }
+            client_socket.send(json.dumps(message_data).encode('utf-8'))
+
+
+name = input("Enter your name: ")
+room = input("Enter the room name: ")
+
+connect_data = {
+    "type": "connect",
+    "payload": {
+        "name": name,
+        "room": room,
+    },
+}
+client_socket.send(json.dumps(connect_data).encode('utf-8'))
+
+send_thread = threading.Thread(target=send_message)
+send_thread.daemon = True
+send_thread.start()
+
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.daemon = True
+receive_thread.start()
+
+send_thread.join()
---
+++ Lab5/Homework/server_hw.py
@@ -0,0 +1,128 @@
+import socket
+import threading
+import json
+import os
+import base64
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+
+connected_clients = {}
+
+def broadcast_message(sender_socket, room, message):
+    for client_socket, client_info in connected_clients.items():
+        if client_info["room"] == room and client_socket != sender_socket:
+            message_data = {
+                "type": "message",
+                "payload": {
+                    "sender": connected_clients[sender_socket]["name"],
+                    "room": room,
+                    "text": message,
+                },
+            }
+            client_socket.send(json.dumps(message_data).encode('utf-8'))
+
+def broadcast_notification(room, message):
+    for client_socket, client_info in connected_clients.items():
+        if client_info["room"] == room:
+            notification_data = {
+                "type": "notification",
+                "payload": {"message": message},
+            }
+            client_socket.send(json.dumps(notification_data).encode('utf-8'))
+
+def handle_image_upload(client_socket, image_name, image_data):
+    try:
+        room_folder = os.path.join("SERVER_MEDIA", connected_clients[client_socket]["room"])
+        if not os.path.exists(room_folder):
+            os.makedirs(room_folder)
+
+        image_path = os.path.join(room_folder, image_name)
+
+        with open(image_path, "wb") as image_file:
+            image_file.write(base64.b64decode(image_data.encode('utf-8')))
+
+        notification_message = f"User {connected_clients[client_socket]['name']} uploaded {image_name}"
+        broadcast_notification(connected_clients[client_socket]["room"], notification_message)
+
+    except Exception as e:
+        print(f"Error handling image upload: {e}")
+
+def handle_image_download(client_socket, image_name):
+    try:
+        room_folder = os.path.join("SERVER_MEDIA", connected_clients[client_socket]["room"])
+        image_path = os.path.join(room_folder, image_name)
+
+        if os.path.exists(image_path):
+            with open(image_path, "rb") as image_file:
+                image_data = base64.b64encode(image_file.read()).decode('utf-8')
+
+            download_data = {
+                "type": "downloaded_image",
+                "payload": {
+                    "image_name": image_name,
+                    "image_data": image_data,
+                }
+            }
+
+            client_socket.send(json.dumps(download_data).encode('utf-8'))
+        else:
+            client_socket.send(f"The {image_name} doesn't exist".encode('utf-8'))
+
+    except Exception as e:
+        print(f"Error handling file download: {e}")
+
+def handle_client(client_socket):
+    try:
+        data = client_socket.recv(1024)
+        connect_data = json.loads(data.decode('utf-8'))
+        name = connect_data["payload"]["name"]
+        room = connect_data["payload"]["room"]
+
+        connected_clients[client_socket] = {"name": name, "room": room}
+
+        notification_message = f"{name} has joined the room."
+        broadcast_message(client_socket, room, notification_message)
+
+        ack_message = {
+            "type": "connect_ack",
+            "payload": {"message": "Connected to the room."}
+        }
+        client_socket.send(json.dumps(ack_message).encode('utf-8'))
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            received_message = json.loads(data.decode('utf-8'))
+            message_type = received_message["type"]
+            if message_type == "message":
+                message_text = received_message["payload"]["text"]
+                broadcast_message(client_socket, room, message_text)
+            elif message_type == "upload":
+                image_name = received_message["payload"]["image_name"]
+                image_data = received_message["payload"]["image_data"]
+                handle_image_upload(client_socket, image_name, image_data)
+            elif message_type == "download":
+                image_name = received_message["payload"]["image_name"]
+                handle_image_download(client_socket, image_name)
+
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        if client_socket in connected_clients:
+            del connected_clients[client_socket]
+        client_socket.close()
+
+server_socket.listen(5)
+print(f"Server is listening on {HOST}:{PORT}")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
